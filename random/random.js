6348ca4c1c2eb636e19940f10101966a0952cd04
guide git <Nht08@NHT102@example.com>
2022-07-29 02:17:57 +0700

    test file

---
+++ random/random.js
@@ -0,0 +1,15 @@
+// document.getElementById('random').onclick = () => {
+//     const valueRandom = Math.random();
+//     const isCheckAttendance = !!Math.round(valueRandom);
+//     if (isCheckAttendance) {
+//         document.getElementsByTagName('h3')[0]
+//             .innerText = 'có'
+//     }
+//     else {
+//         document.getElementsByTagName('h3')[0]
+//             .innerText = 'Không'
+//     }
+// }
+
+var number = Math.sqrt(9)
+console.log(number);
